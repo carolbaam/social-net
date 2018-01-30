7e51b285715422a986b79aee45722565443a4819
carolbaam <carolinabaam@gmail.com>
2018-01-29 19:30:40 -0600

    modificando

--- js/data.js
+++ js/data.js
@@ -1,8 +1,9 @@
 
-var data = [{
+var data = [
+  {
   'type':'exposición Modos de Ver',
   'name':'Museo Carrillo Gil',
-  'image':'../assets/images/modos.png'
+  'image': '../assets/images/modos.png',
   'date':'26 de Enero a 29 de Abril',
   'hour':'10:00 a 18:00 hrs',
   'address':'Av. Revolución 1608, Álvaro Obregón,Ciudad de México, CDMX',
@@ -11,16 +12,16 @@ var data = [{
 {
   'type':'exposición Zapata Doble',
   'name':'Museo de Arte Moderno',
-  'image':'../assets/images/zapata.png'
-  'date':'17 de Noviembre a 18 de Febrero'
-  'hour':'10:15 a 17:30'
+  'image':'../assets/images/zapata.png',
+  'date':'17 de Noviembre a 18 de Febrero',
+  'hour':'10:15 a 17:30',
   'address':'Paseo de la Reforma y Gandhi S/N, Bosque de Chapultepec I Secc,Miguel Hidalgo, CDMX',
   'cost':'medio',
   },
 {
   'type':'exposición Arte / Sano',
   'name':'Museo de arte popular',
-  'image':'../assets/images/sano.png'
+  'image':'../assets/images/sano.png',
   'date':'25 de Noviembre a 25 de Febrero',
   'hour':'10:00 a 18:00 hrs',
   'address':'Revillagigedo 11, Centro,Ciudad de México, CDMX',
@@ -29,7 +30,7 @@ var data = [{
 {
   'type':'Entrecuentos me encuentro ',
   'name':'Foro Shakespeare',
-  'image':'../assets/images/cuentos.png'
+  'image':'../assets/images/cuentos.png',
   'date':'Domignos',
   'hour':'13:30hrs',
   'address':'Zamora 7, Condesa,Ciudad de México, CDMX',
@@ -38,43 +39,43 @@ var data = [{
 {
   'type':'El fantasma en el espejo',
   'name':'López Tarso',
-  'image':'../assets/images/fantasma.png'
+  'image':'../assets/images/fantasma.png',
   'date':'Viernes a Domingo',
-  'hour':'18:00 a 21:30 hrs'
+  'hour':'18:00 a 21:30 hrs',
   'address':'Av. Revolución 1733 Esq. Francisco I. Madero, Col. San Ángel, CDMX',
   'cost':'medio',
   },
 {
   'type':'Los Miserables',
-  'name':'Telcel'
-  'image':'../assets/images/miserables.png'
-  'date':'Proximamente'
-  'hour':'Proximamente'
-  'address':' Miguel de Cervantes Saavedra, Miguel Hidalgo, Granada,Ciudad de México, CDMX'
+  'name':'Telcel',
+  'image':'../assets/images/miserables.png',
+  'date':'Proximamente',
+  'hour':'Proximamente',
+  'address':' Miguel de Cervantes Saavedra, Miguel Hidalgo, Granada,Ciudad de México, CDMX',
   'cost':'Alto',
   },
 {
   'type':'The Realness Tour',
-  'name':'Auditorio BlackBerry'
-  'image':'../assets/images/realness.png'
+  'name':'Auditorio BlackBerry',
+  'image':'../assets/images/realness.png',
   'date':'26 de Mayo',
   'hour':'18:00hrs',
-  'address':´'Tlaxcala 160, Hipódromo Condesa,Ciudad de México, CDMX'
+  'address':'Tlaxcala 160, Hipódromo Condesa,Ciudad de México, CDMX',
   'cost':'medio - alto ',
   },
 {
   'type':'Miranda',
-  'name':'Metropolitan'
-  'image':'../assets/images/miranda.png'
+  'name':'Metropolitan',
+  'image':'../assets/images/miranda.png',
   'date':'19 de Abril',
   'hour':'20:30 hrs',
-  'address':' Av Independencia 90, Colonia Centro, Centro,Ciudad de México, CDMX'
+  'address':' Av Independencia 90, Colonia Centro, Centro,Ciudad de México, CDMX',
   'cost':'medio - alto',
   },
 {
   'type':'Los Estramboticos 25 aniversarios',
   'name':'Pepsi Center',
-  'image':'../assets/images/estramboticos.png'
+  'image':'../assets/images/estramboticos.png',
   'date':'10 de Febrero',
   'hour':'19:00 hrs',
   'address':'Calle Dakota s/n, Benito Juarez, Nápoles,Ciudad de México, CDMX',
@@ -82,21 +83,21 @@ var data = [{
   },
 {
   'type':'Elixir de amor',
-  'name':'Auditorio Nacional'
-  'image':'../assets/images/elixir.png'
-  'date':'10 de Febrero'
-  'hour':'10:55 '
-  'address':
+  'name':'Auditorio Nacional',
+  'image':'../assets/images/elixir.png',
+  'date':'10 de Febrero',
+  'hour':'10:55 ',
+  'address':'Calle Reforma s/n Miguel Hidalgo,Ciudad de México, CDMX',
   'cost':'medio',
   },
 {
   'type':'Rice',
-  'name':'Palacio de Bellas Atess'
-  'image':'../assets/images/rice.png'
-  'date':'16 y 17 de Febrero'
-  'hour':'19:00 y 20:00 hrs'
-  'address':'Av. Juárez, Centro Histórico,Ciudad de México, CDMX'
+  'name':'Palacio de Bellas Atess',
+  'image':'../assets/images/rice.png',
+  'date':'16 y 17 de Febrero',
+  'hour':'19:00 y 20:00 hrs',
+  'address':'Av. Juárez, Centro Histórico,Ciudad de México, CDMX',
   'cost':'medio',
-  },
+  }
 
-}]
+];
